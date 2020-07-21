45c88f615b39d694ce6108e4d85dfdeb8d8fd8ff
genhow <genhow2000@gmail.com>
2020-07-21 17:32:39 +0800

    t

---
+++ src/Flower/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lingan
+ * Date: 2017/11/6
+ * Time: 21:38
+ */
+
+namespace Flower\Lily;
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose..php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lingan
+ * Date: 2017/11/6
+ * Time: 21:39
+ */
+
+namespace Flower\Rose;
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
