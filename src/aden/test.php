1c00c888ef2cd3d4a0b0fe01043da46aeecb86ba
genhow <genhow2000@gmail.com>
2020-07-21 17:17:00 +0800

    test

---
+++ src/aden/test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lingan
+ * Date: 2017/11/6
+ * Time: 21:38
+ */
+
+namespace src\aden;
+
+class test
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
